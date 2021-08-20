7c2f7e34ceecfcd81727e6b703ee3450e3f74893
johncao1 <64365529+johncao1@users.noreply.github.com>
2021-08-19 22:04:11 -0700

    Update playground.js

--- src/playground/playground.js
+++ src/playground/playground.js
@@ -6,18 +6,7 @@ import factorial from '../algorithms/math/factorial/factorial';
 // Launch playground tests by running: npm test -- 'playground'
 function playground() {
   // Replace the next line with your playground code.
-  Add(value)
-  Pre: value is the value to add to the list
-  Post: value has been placed at the tail of the list
-  n ← node(value)
-  if head = ø
-    head ← n
-    tail ← n
-  else
-    tail.next ← n
-    tail ← n
-  end if
-end Add
+  
 }
 
 export default playground;
